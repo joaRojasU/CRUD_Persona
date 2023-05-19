920289040a98433c90eef8cd4d9da317553f3d38
joarojasu <joarojas@duocuc.cl>
2023-05-19 09:27:51 -0400

    backup

--- CRUD_Persona/Models/ListPersonaViewModel.cs
+++ CRUD_Persona/Models/ListPersonaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace CRUD_Persona.Models
     public class ListPersonaViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Rut { get; set; }
+        [Required]
         public string Nombre { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Departamento { get; set; }
+        [Required]
         public string Telefono { get; set; }
 
     }
